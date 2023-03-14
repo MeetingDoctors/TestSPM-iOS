cc73834cd96d17e578c09be78a457c982cc94bb3
Carles Cañadas Torrents <>
2023-03-14 15:57:33 +0100

    feat: add support to ios13,

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "TestSPM",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -59,9 +62,6 @@ let package = Package(
         .binaryTarget(
             name: "MeetingDoctorsSDK",
             path: "Frameworks/MeetingDoctorsSDK.xcframework"
-        ),
-        .testTarget(
-            name: "TestSPMTests",
-            dependencies: ["TestSPM"]),
+        )
     ]
 )
