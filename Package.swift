d7c15da57ec8f334f6812908490efe9595a47a56
Carles Cañadas Torrents <>
2024-02-29 16:43:52 +0100

    feat: update with all targets

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MeetingDoctors-Core",
+    name: "MeetingDoctorsSDK",
     platforms: [
         .iOS(.v13),
     ],
@@ -14,9 +14,39 @@ let package = Package(
             name: "MeetingDoctorsCore",
             targets: ["MeetingDoctorsCore"]
         ),
+        .library(
+            name: "MeetingDoctorsSchema",
+            targets: ["MeetingDoctorsSchema"]
+        ),
+        .library(
+            name: "MeetingDoctorsController",
+            targets: ["MeetingDoctorsController"]
+        ),
+        .library(
+            name: "MeetingDoctorsSocket",
+            targets: ["MeetingDoctorsSocket"]
+        ),
+        .library(
+            name: "MeetingDoctorsStorage",
+            targets: ["MeetingDoctorsStorage"]
+        ),
+        .library(
+            name: "MeetingDoctorsRemote",
+            targets: ["MeetingDoctorsRemote"]
+        ),
+        .library(
+            name: "MeetingDoctorsSDK",
+            targets: ["MeetingDoctorsSDK"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.5.0"),
+        .package(url: "https://github.com/socketio/socket.io-client-swift.git", from: "15.2.0"),
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "10.17.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.3.0"),
+        .package(url: "https://github.com/opentok/vonage-client-sdk-video", from: "2.26.0"),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,18 +56,31 @@ let package = Package(
             name: "MeetingDoctorsCore",
             path: "Frameworks/MeetingDoctorsCore.xcframework"
         ),
-        .target(
-            name: "MeetingDoctorsControllerWrapper",
-            dependencies: [
-                .target(name: "MeetingDoctorsCore"),
-            ],
-            path: "Sources/MeetingDoctorsControllerWrapper",
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("UIKit"),
-                .linkedFramework("CoreLocation"),
-                .linkedFramework("AVFoundation"),
-            ]
+        // MARK: - MDSchema
+        .binaryTarget(
+            name: "MeetingDoctorsSchema",
+            path: "Frameworks/MeetingDoctorsSchema.xcframework"
         ),
+        // MARK: - MDController
+        .binaryTarget(
+            name: "MeetingDoctorsController",
+            path: "Frameworks/MeetingDoctorsController.xcframework"
+        ),
+        .binaryTarget(
+            name: "MeetingDoctorsSocket",
+            path: "Frameworks/MeetingDoctorsSocket.xcframework"
+        ),
+        .binaryTarget(
+            name: "MeetingDoctorsStorage",
+            path: "Frameworks/MeetingDoctorsStorage.xcframework"
+        ),
+        .binaryTarget(
+            name: "MeetingDoctorsRemote",
+            path: "Frameworks/MeetingDoctorsRemote.xcframework"
+        ),
+        .binaryTarget(
+            name: "MeetingDoctorsSDK",
+            path: "Frameworks/MeetingDoctorsSDK.xcframework"
+        )
     ]
 )
